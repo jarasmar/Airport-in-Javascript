8814183b34a00417cd5bbc844a0f5a1de98d52b9
Jara Santamaria <jarasmar@gmail.com>
2020-03-17 12:35:40 +0000

    Takeoff method created

--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -7,12 +7,18 @@ describe('Plane', function() {
   beforeEach(function() {
     plane = new Plane();
     // Jasmine Mocking, creates a double of a different class //
-    airport = jasmine.createSpyObj('airport', ['clearForLanding']);
+    airport = jasmine.createSpyObj('airport', ['clearForLanding', 'clearForTakeoff']);
   });
 
   it('can land at an airport', function() {
     // stubbing the outgoing interaction from plane to airport
     plane.land(airport);
     expect(airport.clearForLanding).toHaveBeenCalledWith(plane);
+  });
+
+  it('can takeoff from an airport', function() {
+    plane.land(airport);
+    plane.takeoff();
+    expect(airport.clearForTakeoff).toHaveBeenCalled();
   });
 });
--- src/Plane.js
+++ src/Plane.js
@@ -5,4 +5,9 @@ function Plane() {}
 Plane.prototype.land = function(airport) {
   // plane talk to the airport to ask to land:
   airport.clearForLanding(this);
+  this._location = airport;
+};
+
+Plane.prototype.takeoff = function() {
+  this._location.clearForTakeoff();
 };
